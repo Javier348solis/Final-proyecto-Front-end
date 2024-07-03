5d73a32e6407d0b048aa27c9a4010aad8fb748d2
Javier348solis <javiersolisretana@gmail.com>
2024-07-03 04:26:48 -0600

    Imagenes pie de pagina

--- src/components/PiedePagina.jsx
+++ src/components/PiedePagina.jsx
@@ -4,8 +4,9 @@ function PiedePagina() {
   return (
     <>
      <footer>
-    
-    
+    <img src="src/Images/facebook (1).png" alt="" width={50} height={50}/>
+    <img src="src/Images/social (1).png" alt="" width={50} height={50}/>
+    <img src="src/Images/whatsapp.png" alt="" width={50} height={50}/>
     </footer> 
     </>
   )
