0e3c48fd713bda0dabab8f90306e06f6ca80e84d
Javier348solis <javiersolisretana@gmail.com>
2024-07-07 16:43:58 -0600

    Cambios en el db.json

--- src/BarraNavBar/Contacto.jsx
+++ src/BarraNavBar/Contacto.jsx
@@ -7,7 +7,8 @@ function Contacto() {
       <input type="number" placeholder='Telefono Completo'/>
       <input type="email" placeholder='Correo Electronico'/>
       <input type="text" placeholder='Mensaje'/>
-      <button>Enviar</button> 
+      <button onClick={Contacto}>Enviar</button> 
+      
       </div>
 <br/>
 <h4>*Nota: Las dudas o consultas se responderan en un lapso de 12 a 24 horas por correo electronico o WhatsApp. Gracias por su preferencia!</h4>
--- src/components/PiedePagina.jsx
+++ src/components/PiedePagina.jsx
@@ -5,12 +5,12 @@ function PiedePagina() {
   return (
     <>
      <footer>
-    <img src="src/Images/facebook (1).png" alt="" width={50} height={50}/>
-    <img src="src/Images/social (1).png" alt="" width={50} height={50}/>
-    <img src="src/Images/whatsapp.png" alt="" width={50} height={50}/>
+    <img onClick={PiedePagina} src="src/Images/facebook (1).png" alt="" width={50} height={50}/>
+    <img onClick={PiedePagina} src="src/Images/social (1).png" alt="" width={50} height={50}/>
+    <img onClick={PiedePagina} src="src/Images/whatsapp.png" alt="https://web.whatsapp.com/" width={50} height={50}/>
     </footer> 
     </>
-  )
+  )  
 }
 
 export default PiedePagina
