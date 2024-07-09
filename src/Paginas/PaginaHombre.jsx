0c18dc4ab459a2b78224f235cc87e87b0ee5c1d9
Javier348solis <javiersolisretana@gmail.com>
2024-07-09 09:32:00 -0600

    Div men's page

--- src/Paginas/PaginaHombre.jsx
+++ src/Paginas/PaginaHombre.jsx
@@ -4,10 +4,10 @@ import CardFragance from '../components/CardFragance'
 function PaginaHombre() {
   return (
     <div className='Fotos-Hombre' >
-      <CardFragance src={"src/Images/Azzaro.jpg"} titulo={"A01"} precio={"30.000"}/>
-      <CardFragance src={"src/Images/profondo.jpg"} titulo={"A01"} precio={"30.000"}/>
-      <CardFragance src={"src/Images/Invictus.jpg"} titulo={"A01"} precio={"30.000"}/>
-      <CardFragance src={"src/Images/One.jpg"} titulo={"A01"} precio={"30.000"}/>
+      <CardFragance src={"src/Images/Azzaro.jpg"} titulo={"Azzaro wanted EDT"} precio={"₡55.000"}/>
+      <CardFragance src={"src/Images/profondo.jpg"} titulo={"ADG Profondo Parfum"} precio={"₡80.000"}/>
+      <CardFragance src={"src/Images/Invictus.jpg"} titulo={"Invictus Parfum PR"} precio={"₡65.000"}/>
+      <CardFragance src={"src/Images/One.jpg"} titulo={"One Million PR EDT"} precio={"₡44.000"}/>
     </div>
   )
 }
--- src/components/CardFragance.jsx
+++ src/components/CardFragance.jsx
@@ -2,7 +2,7 @@ const CardFragance=({src,titulo,precio})=>{
     return(
         <>
         <div>
-          <img width={"200px"} src={src} alt="" /> 
+          <img width={"200px"} height={"200"} src={src} alt="" /> 
           <h2 className="text-success">{titulo}</h2>
           <span className="text-info">{precio}</span>
           </div>
